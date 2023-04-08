21c9b56f81e56524a3d7b1d2bb4c48deeed58bbd
Jotaro <zibble@mail.ru>
2023-04-08 10:12:15 +0400

    Initial commit

---
+++ core/src/com/avozhov/som/Neuron.java
@@ -0,0 +1,29 @@
+package com.avozhov.som;
+
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.Random;
+
+public class Neuron {
+    int x;
+    int y;
+    float[] w;
+    Color color;
+    public Neuron(int n, int x, int y) {
+        this.x = x;
+        this.y = y;
+        w = new float[n];
+        for (int i = 0; i < n; i++)
+            w[i] = new Random().nextFloat(-1, 1);
+        color = new Color(1, 1, 1, 1);
+    }
+
+    public void recolor() {
+        if(w.length == 1)
+            color = new Color(w[0]*1, w[0]*1, w[0]*1, 1);
+        if(w.length == 2)
+            color = new Color(w[0]*1, w[1]*1, 1, 1);
+        if(w.length >= 3)
+            color = new Color(w[0]*1, w[1]*1, w[2]*1, 1);
+    }
+}
